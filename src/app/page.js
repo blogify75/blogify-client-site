bc717bed9b2a761f22f6e628819718cbf0b16a08
akilinjamam <akilinjamam@gmail.comm>
2023-12-11 11:44:33 +0600

    file-updated

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main>
       <Intro />
-      <LatestBlogNews />
+      {/* <LatestBlogNews /> */}
       <Deals />
       <About />
       <Service />
