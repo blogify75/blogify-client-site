737034107dfd97cd332ffc247bb92f515169665c
akilinjamam <akilinjamam@gmail.comm>
2023-12-18 09:58:32 +0600

    file-udpated

--- src/app/homeComponents/navbar/Navbar.jsx
+++ src/app/homeComponents/navbar/Navbar.jsx
@@ -1,18 +1,21 @@
+"use client"
 import nav from './Navbar.module.css';
 import logo from '../../../images/blogify-new.png';
 import Image from 'next/image';
 import Link from 'next/link';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from 'react';
 
 const Navbar = () => {
-  
+    const [open , setOpen] = useState(false);
     return (
        <div>
          <div className={`${nav.main}`}>
             <div className={`${nav.container}`}>
-                <div className={`${nav.left}  flex `}>
+                <div className={`${nav.left}  flex`}>
                     <Image src={logo} width={200} height={80} alt='blogify' />
                 </div>
-                <div className={`${nav.middle}  flex `}>
+                <div className={`${nav.middle}  flex  `}>
                     <div className={`${nav.middleContainer} flex `}>
                         <ul className={`only_flex`}>
                             <Link style={{textDecoration:'none', color:'black', marginRight:'25px'}} href='/'>HOME</Link>
@@ -37,6 +40,48 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
+            <div className={`${nav.containerRes}`}>
+                <div className={`${nav.leftRes}  flex`}>
+                    <Image src={logo} width={180}  alt='blogify' />
+                </div>
+                <div style={{position:'relative'}} className={`${nav.middleRes}  flex_end `}>
+                    <div className={`${nav.middleContainerRes} flex `}>
+                        <p style={{cursor:'pointer'}} onClick={() => setOpen(!open)}>< RxHamburgerMenu /></p>
+                    </div>
+                    <div style={{
+                        width:'200px',
+                        height:'300px',
+                        backgroundColor:'rgba(27, 27, 27, 0.91)',
+                        borderRadius:'10px',
+                        position:'absolute',
+                        top:'80px',
+                        right:'70px',
+                        zIndex:'14',
+                        padding:'0 10px',
+                        display: `${open ? 'block': 'none'}`
+                    }}>
+                         <ul>
+                            <Link style={{textDecoration:'none', color:'white', marginRight:'25px', fontSize:'13px'}} href='/'>HOME</Link>
+                            <br />
+                            <br />
+                            <Link style={{textDecoration:'none', color:'white', marginRight:'25px', fontSize:'13px'}} href=''>ABOUT US</Link>
+                            <br />
+                            <br />
+                            <Link style={{textDecoration:'none', color:'white', marginRight:'25px', fontSize:'13px'}} href='/deals'>DEALS & SHOPPING</Link>
+                            <br />
+                            <br />
+                            <Link style={{textDecoration:'none', color:'white', marginRight:'25px', fontSize:'13px'}} href='/blogs'>BLOGS</Link>
+                            <hr />
+                            <br />
+                            <Link style={{textDecoration:'none', color:'white', marginRight:'25px', fontSize:'13px'}} href='https://dashboard.blogify.life/' target='__blank'>SIGN IN</Link>
+                            <br />
+                            <br />
+                            <Link style={{textDecoration:'none', color:'white', marginRight:'25px', fontSize:'13px'}}  href='https://dashboard.blogify.life/signup' target='__blank'>SIGN UP</Link>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
         </div>
         <div className={`${nav.footer}`}>
             <div className={`${nav.footerContainer}`}>
@@ -57,6 +102,33 @@ const Navbar = () => {
                 </div>
                 <div>
                     <Link style={{textDecoration:'none', color:'white'}} href="/kidsProducts">Kids Products</Link>
+                </div>
+                <div>
+                    <Link style={{textDecoration:'none', color:'white'}} href="/deals">Latest Deals</Link>
+                </div>
+            </div>
+            <div className={`${nav.footerContainerRes}`}>
+               <div>
+                <div>
+                    <Link style={{textDecoration:'none', color:'white'}} href="/blogs">Latest Blogs</Link>
+                    </div>
+                    <div>
+                    <Link style={{textDecoration:'none', color:'white'}} href="/personalCare">Personal Care</Link>
+                    </div>
+                    <div>
+                        <Link style={{textDecoration:'none', color:'white'}} href="/beautyAndFashion">Beauty & Fashion</Link>
+                    </div>
+               </div>
+                <div>
+                    <div>
+                        <Link style={{textDecoration:'none', color:'white'}} href="/petLife">Pet life</Link>    
+                    </div>
+                    <div>
+                        <Link style={{textDecoration:'none', color:'white'}} href="/social">Social</Link>    
+                    </div>
+                    <div>
+                        <Link style={{textDecoration:'none', color:'white'}} href="/kidsProducts">Kids Products</Link>
+                    </div>
                 </div>
                 <div>
                     <Link style={{textDecoration:'none', color:'white'}} href="/deals">Latest Deals</Link>
