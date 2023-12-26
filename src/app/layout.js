3f877676258db422c0a3f21f3116715d62781ca0
akilinjamam <akilinjamam@gmail.comm>
2023-12-27 00:15:00 +0600

    file-updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/src/app/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
